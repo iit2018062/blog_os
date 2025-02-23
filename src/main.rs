598b6735780bf523f756ee24e3ac470e7365c63c
iit2018062 <iit2018062@iiita.ac.in>
2025-02-22 22:46:38 -0800

    post 2

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,19 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
 /// This is the entry point of the kernel, called by the bootloader.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     // This replaces the main function.
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
