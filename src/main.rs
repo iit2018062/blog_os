14e7623a9312ade1970f0229d76e721e8a9a7566
iit2018062 <iit2018062@iiita.ac.in>
2025-03-08 23:48:48 -0800

    added vga tests

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,74 @@
 #![no_std]
 #![no_main]
 
-mod vga_buffer;
-
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 use core::panic::PanicInfo;
+mod serial;
+mod vga_buffer;
+pub trait Testable {
+    fn run(&self) -> ();
+}
 
-/// This function is called on panic.
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+
+
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
-static HELLO: &[u8] = b"Hello World!";
+//static HELLO: &[u8] = b"Hello World!";
 /// This is the entry point of the kernel, called by the bootloader.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Some panic message");
+    #[cfg(test)]
+    test_main();
+    #[deny(clippy::empty_enum)]
 
     loop {}
+}
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Testable]) { // new
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run(); // new
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+#[test_case]
+fn trivial_assertion() {
+    // serial_println!("trivial assertion... ");
+    assert_eq!(1, 1);
+    // serial_println!("[ok]");
 }
