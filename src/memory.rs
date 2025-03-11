903c3c04ec84afd16031070923f15e7e20f8b8bc
iit2018062 <iit2018062@iiita.ac.in>
2025-03-10 22:27:21 -0700

    Choosing a Virtual Page

--- src/memory.rs
+++ src/memory.rs
@@ -4,6 +4,11 @@ use x86_64::{
     structures::paging::PageTable,
     VirtAddr,
 };
+use x86_64::{
+    structures::paging::{Page, PhysFrame, Mapper, Size4KiB, FrameAllocator}
+};
+use x86_64::structures::paging::OffsetPageTable;
+
 /// Private function that is called by `translate_addr`.
 ///
 /// This function is safe to limit the scope of `unsafe` because Rust treats
@@ -71,4 +76,32 @@ pub unsafe fn translate_addr(addr: VirtAddr, physical_memory_offset: VirtAddr)
                              -> Option<PhysAddr>
 {
     translate_addr_inner(addr, physical_memory_offset)
+}
+/// Creates an example mapping for the given page to frame `0xb8000`.
+pub fn create_example_mapping(
+    page: Page,
+    mapper: &mut OffsetPageTable,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) {
+    use x86_64::structures::paging::PageTableFlags as Flags;
+
+    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+    let flags = Flags::PRESENT | Flags::WRITABLE;
+
+    let map_to_result = unsafe {
+        // FIXME: this is not safe, we do it only for testing
+        mapper.map_to(page, frame, flags, frame_allocator)
+    };
+    map_to_result.expect("map_to failed").flush();
+}
+pub struct EmptyFrameAllocator;
+
+unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        None
+    }
+}
+pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+    let level_4_table = active_level_4_table(physical_memory_offset);
+    OffsetPageTable::new(level_4_table, physical_memory_offset)
 }
